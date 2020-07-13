d468c87b4b30b5bf7a171d3d98bdf15653566653
Peter Konovodov <p_konovodov@mail.ru>
2020-07-13 14:36:44 +0500

    final

--- app/src/main/java/com/example/hw332/MainActivity.java
+++ app/src/main/java/com/example/hw332/MainActivity.java
@@ -2,18 +2,19 @@ package com.example.hw332;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
-import android.widget.TextView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     private Spinner languageSpinner;
-    TextView infobox;
     Button okBtn;
 
     @Override
@@ -21,8 +22,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         languageSpinner = findViewById(R.id.languagesSpinner);
-        infobox = findViewById(R.id.infobox);
         okBtn = findViewById(R.id.okBtn);
+        okBtn.setOnClickListener(onClickListener);
+        initSpinner();
+
 
     }
 
@@ -40,5 +43,33 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+Button.OnClickListener onClickListener = new Button.OnClickListener() {
+
+    @Override
+    public void onClick(View v) {
+        Locale locale;
+        switch ((int) languageSpinner.getSelectedItemPosition()) {
+            case 1:
+                locale = new Locale("ru");
+                break;
+            case 2:
+                locale = new Locale("ch");
+                break;
+            case 3:
+                locale = new Locale("tt");
+                break;
+            default:
+                locale = new Locale("en");
+                break;
+        }
+        Configuration config = new Configuration();
+        config.setLocale(locale);
+        getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+        recreate();
+
+    }
+};
+
 
 }
